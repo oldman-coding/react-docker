b5104e4ffb2b8d582995109df7fe65a4d1357db2
crmviet <ng.manhtuong@gmail.com>
2021-06-20 09:19:33 +0700

    test unit test fail

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello ReactJs!
+          Hello Re
         </p>
         <h1>Youtube channel for engineer</h1>
         
